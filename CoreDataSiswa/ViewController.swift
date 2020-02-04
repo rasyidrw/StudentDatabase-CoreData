af15bd79025bae0a93522fd18e338fb693133ab3
rasyidrw <rasyidrespati.30@gmail.com>
2020-02-04 10:35:29 +0700

    Initial Commit

---
+++ CoreDataSiswa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataSiswa
+//
+//  Created by Rasyid Respati Wiriaatmaja on 04/02/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
